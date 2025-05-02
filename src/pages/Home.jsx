9fa8dd64041c0e4339f107e9e53e8a757a1cc77c
MaxShmih <shmihel.maksym@gmail.com>
2025-05-02 14:36:55 +0300

    Test husky pre-commit hook

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import LanguageSwitcher from '../components/widgets/LanguageSwitcher';
 
 function Home() {
   const { t } = useTranslation();
-  const unusedVar = 42;
+  const unusedVar = 43;
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
       <LanguageSwitcher />
